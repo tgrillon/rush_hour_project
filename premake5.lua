70511c0b660f7a03db9b14647cd982ddef549115
To <theogrillon6f9@gmail.com>
2023-04-10 15:48:49 +0200

    Update premake

--- premake5.lua
+++ premake5.lua
@@ -3,28 +3,32 @@ workspace "Rush-hour"
   architecture "x86_64"
   location "build"
 
-  project "Rush-hour"
+  filter "configurations:Debug"
+    defines { "DEBUG" }
+    symbols "On"
+
+  filter "configurations:Release"
+    defines { "NDEBUG" }
+    optimize "On"
+
+  configuration { "windows", "vs*" }
+    location "build"
+    debugdir "."
+
+  project "Rush-hour-test"
     kind "ConsoleApp"
     language "C++"
     targetdir "bin/%{cfg.buildcfg}"
     objdir "obj/%{cfg.buildcfg}"
+    files { "Code/game_situation.cpp", "Code/game_situation.h", "Code/main.cpp" }
 
-    files { "Code/**.cpp", "Code/**.h" }
-    includedirs { "Code" }
-
+  project "Rush-hour-sdl"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir "obj/%{cfg.buildcfg}"
+    files { "Code/game_situation.cpp", "Code/game_situation.h", "Code/main_sdl.cpp" }
     includedirs { "include" }
     libdirs { "lib/x64" }
     links { "SDL2.lib", "SDL2main.lib", "SDL2_image.lib" }
     links { "SDL2", "SDL2main", "SDL2_image"}
-
-    filter "configurations:Debug"
-      defines { "DEBUG" }
-      symbols "On"
-
-    filter "configurations:Release"
-      defines { "NDEBUG" }
-      optimize "On"
-
-    configuration { "windows", "vs*" }
-      location "build"
-      debugdir "."
